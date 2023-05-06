b34d3d80d6e465489dfe2309ff6932996b9f639a
Solomon Eniola Adegoke <Souceking@gmail.com>
2023-05-06 20:38:12 +0100

    serrializer

---
+++ serializerss/apiserializer/serializeapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Category(models.Model):
+    id = models.IntegerField(primary_key=True)
+    slug = models.SlugField()
+    title = models.CharField(max_length=255)
+
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
---
+++ serializerss/apiserializer/serializeapp/views.py
@@ -0,0 +1,24 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+from django.shortcuts import get_object_or_404
+
+@api_view(['GET', 'POST'])
+def menu_items(request):
+    if request.method == 'GET':
+        items = MenuItem.objects.select_related('category').all()
+        serialized_item = MenuItemSerializer(items, many=True)    
+        return Response(serialized_item.data)
+    if request.method == 'POST':
+        serialized_item = MenuItemSerializer(data=request.data)
+        serialized_item.is_valid(raise_exception=True)
+        serialized_item.save()
+        return Response(serialized_item.data, status.HTTP_201_CREATED)
+
+@api_view()
+def single_item(request, id):
+    # item  =MenuItem.objects.get(pk=id)
+    item = get_object_or_404(MenuItem, pk=id)
+    serialized_item = MenuItemSerializer(item)
+    return Response(serialized_item.data)
